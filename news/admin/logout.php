79368b2f23d592928fff876a2ac1eb21a15f454f
nunavx <35895294+nunavx@users.noreply.github.com>
2018-02-07 20:15:11 +0700

    Move to WordPress CMS
    Move to WordPress CMS, its more easy to edit and delete.

--- news/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['kosong']); 
-header("location:admlogin.php");
-?>
--- page/news.php
+++ page/news.php
@@ -2,23 +2,83 @@
 <div class="post"><div class="top"/>
 
 <style>
+.accordion {
+    background-color: #3c3c3c;
+    color: #fff;
+    cursor: pointer;
+    padding: 15px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    transition: 0.4s;
+	margin-top: 15px;
+}
 
+.active, .accordion:hover {
+    background-color: #87CEEB;
+	color: #3c3c3c;
+}
+
+.accordion:after {
+    content: '\002B';
+	position: absolute;
+	right: 15px;
+    color: #fff;
+    font-weight: bold;
+    float: right;
+    margin-left: 5px;
+}
+
+.active:after {
+    content: "\2212";
+}
+.date-right {
+	float: right;
+	margin-right: 20px;
+	font-size: 12pt;
+}
+.panel {
+    padding: 0 18px;
+    background-color: white;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.2s ease-out;
+}
 </style>
+</br>
+<h1><u>Latest News</u></h1>
 <?php
-
 mysql_connect('localhost','root','your password');
-mysql_select_db('website');
-$query = mysql_query('SELECT * FROM berita ORDER BY id DESC');
+mysql_select_db('wordpress');
+$query = mysql_query('SELECT * FROM wp_posts WHERE post_status="publish" ORDER BY post_date DESC');
 while($output = mysql_fetch_assoc($query))
 { 
-	$judul = '<h2>'.$output['title'].'</h2>';
-	echo $judul;
-	$topik = $output['content'];
-	echo $output['image'];
-	echo $topik .'<br / >';
-	echo $output['date']).'<br / >';
-	echo '<hr />'; 
+	
+	$title = '<button class="accordion">'.$output['post_title'].'<div class="date-right">'.date('d-m-Y', strtotime($output['post_date'])).'</div></button>';
+	echo $title;
+	$topik = $output['post_content'];
+	echo '<div class="panel"><br>'.$topik;
+	echo '<br><hr /><center>© 2018 NUNA <br> Contact me at nunavx.rf@gmail.com</center><br></div>'; 
 }
 ?>
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+</script>
+
 </div>
 <!-- news section -->
